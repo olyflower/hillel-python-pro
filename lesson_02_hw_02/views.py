536e969a81072d8b1cd6ae79b8e1a58327900753
olyflower <2013.olga.yudina@gmail.com>
2022-11-24 22:27:43 +0200

    Added function gen_password_easy, calculate_average_pandas

--- lesson_02_hw_02/views.py
+++ lesson_02_hw_02/views.py
@@ -3,8 +3,8 @@
 2. def calculate_average()
 """
 
+import pandas as pd
 import random
-from statistics import mean
 import csv
 from flask import Flask
 app = Flask(__name__)
@@ -31,17 +31,36 @@ def gen_password():
     return f'Password: {password}'
 
 
+@app.route("/password_easy")
+def gen_password_easy():
+    """ Function to generate password.
+    """
+    lst = list('!*ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')
+    random.shuffle(lst)
+    password = ''.join(random.choice(lst) for _ in range(10, 21))
+    return f'Password: {password}'
+
+
 @app.route("/average")
 def calculate_average(path='hw.csv'):
     """ Function to calculate average height and weight.
     """
-    with open(path, 'r') as f:
-        reader = csv.DictReader(f)
-        average_high = mean([float(row[' Height(Inches)']) for row in reader])
-    with open(path, 'r') as f:
-        reader = csv.DictReader(f)
-        average_weight = mean([float(row[' Weight(Pounds)']) for row in reader])
+    with open(path) as f:
+        reader = csv.DictReader(f, delimiter=",")
+        all_heights = []
+        all_weights = []
+        for row in reader:
+            all_heights.append(float(row[" Height(Inches)"]))
+            all_weights.append(float(row[" Weight(Pounds)"]))
+        average_high = sum(all_heights) / len(all_heights)
+        average_weight = sum(all_weights) / len(all_weights)
     return f'Average high: {average_high} (inches), Average weight : {average_weight} (pounds)'
+
+
+@app.route("/average_pandas")
+def calculate_average_pandas():
+    df = pd.read_csv('hw.csv', index_col=0)
+    return f'{df.mean(axis=0)}'
 
 
 app.run(port=5000, debug=True)
