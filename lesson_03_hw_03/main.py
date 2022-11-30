681f8721eb52a6931fe9b49a6a3c04582e146ac7
olyflower <2013.olga.yudina@gmail.com>
2022-11-30 23:50:20 +0200

    Made changes

--- lesson_03_hw_03/main.py
+++ lesson_03_hw_03/main.py
@@ -4,11 +4,10 @@
 """
 
 import csv
-import pandas
 import requests
 
 from faker import Faker
-from flask import Flask, request
+from flask import Flask
 from webargs import fields, validate
 from webargs.flaskparser import use_kwargs
 
@@ -21,7 +20,7 @@ faker_instance = Faker()
 @use_kwargs(
     {
         'count': fields.Int(
-            missing=5,
+            missing=10,
             validate=[validate.Range(min=1, max=1000)]
         )
     },
@@ -30,32 +29,36 @@ faker_instance = Faker()
 def generate_students(count):
     """Function generates student first name, last name, email, password, date of birth.
     """
-    count = request.args.get('count', default='10')
-    count = int(count)
     with open('students.csv', 'w') as file:
         writer = csv.writer(file)
         writer.writerow(['First_name', 'Last_name', 'Email', 'Password', 'Date_of_Birth'])
         for _ in range(count):
             writer.writerow([faker_instance.first_name(), faker_instance.last_name(), faker_instance.email(),
                              faker_instance.password(), faker_instance.date_of_birth()])
-    return str(pandas.read_csv('students.csv'))
+    with open('students.csv', 'r') as file:
+        return file.read().replace('\n\n', '<br>')
 
 
-@app.route("/bitcoin_rate")
-def get_bitcoin_value():
+@app.route("/bitcoin-rate")
+@use_kwargs(
+    {
+        'count': fields.Int(
+            missing=1
+        ),
+        'currency': fields.Str(
+            load_default='USD'
+        )
+    },
+    location='query'
+)
+def get_bitcoin_value(count, currency):
     """Function get value currency of bitcoin, symbol of input currency code and quantity of bitcoins.
     """
-    bitcoin_value = 0
     symbol_currency_code = 0
-    currency = request.args.get('currency', default='USD')
-    count = request.args.get('count', default=1)
-    count = int(count)
-    for i in requests.get('https://bitpay.com/api/rates').json():
+    bitcoin_value = requests.get('https://bitpay.com/api/rates/' + currency).json()['rate']
+    for i in requests.get('https://bitpay.com/currencies').json()['data']:
         if i['code'] == currency:
-            bitcoin_value = i['rate']
-    for j in requests.get('https://bitpay.com/currencies').json()['data']:
-        if j['code'] == currency:
-            symbol_currency_code = j['symbol']
+            symbol_currency_code = i['symbol']
     bitcoins = int(count / bitcoin_value)
     return f'Bitcoin value: {bitcoin_value}, Symbol of input currency code: {symbol_currency_code},' \
            f' Quantity of bitcoins: {bitcoins}'
